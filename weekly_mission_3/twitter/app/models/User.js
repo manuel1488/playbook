efe471b9e42ec99f21826dded8eb35f3ef1f178e
Manuel Alfaro <manuel.alfaro1488@gmail.com>
2022-04-24 12:13:09 -0500

    Models test driven develoment twitter

---
+++ weekly_mission_3/twitter/app/models/User.js
@@ -0,0 +1,39 @@
+class User{
+    constructor(id, username, name, bio){
+        this.id = id
+        this.username = username
+        this.name = name
+        this.bio = bio
+        this.dateCreated = new Date()
+        this.lastUpdated = new Date()
+    }
+    
+    getUsername(){
+        return this.name
+    }
+
+    getBio(){
+        return this.bio
+    }
+
+    getDateCreated(){
+        return this.dateCreated
+    }
+
+    getLastUpdated(){
+        return this.lastUpdated
+    }
+
+    set setUsername(username){
+        this.username = username
+        this.lastUpdated = Date.now()
+    }
+
+    set setBio(bio){
+        this.bio = bio
+    }
+    
+}
+
+
+module.exports = User
---
+++ weekly_mission_3/twitter/test/models/User.test.js
@@ -0,0 +1,23 @@
+const User = require('./../../app/models/User')
+
+describe('Unite test for class user', () => {
+    test('Create a user object', () => {
+        const user = new User(1, 'manuelalfaro', 'manuel', 'Bio')
+
+        expect(user.id).toBe(1)
+        expect(user.username).toBe('manuelalfaro')
+        expect(user.name).toBe('manuel')
+        expect(user.bio).toBe('Bio')
+        expect(user.dateCreated).not.toBeUndefined()
+        expect(user.lastUpdated).not.toBeUndefined()
+    });
+
+    test('test setters', () => {
+        const user = new User(1, 'manuelalfaro', 'manuel', 'Bio')
+        user.setUsername = 'manuel'
+        expect(user.username).toBe('manuel')
+
+        user.setBio = 'New bio'
+        expect(user.bio).toBe('New bio')
+    });
+});
